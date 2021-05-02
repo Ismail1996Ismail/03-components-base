b1f1808cfc77620cc8224a2d3e4b51c6465b13ee
ismail <ismail.buev@mail.ru>
2021-05-02 12:00:10 +0300

    react-3

---
+++ react-3 iz/src/Base.js
@@ -0,0 +1,10 @@
+import World from "./earch.jpg";
+function Base(){
+    return (
+        <div className="base">
+            <h>Hello World</h>
+            <img src={World} alt="world" />
+        </div>
+    )
+};
+export default Base;
